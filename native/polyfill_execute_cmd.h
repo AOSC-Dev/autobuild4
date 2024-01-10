4927fd3f5490f6138a1aaf41e9141b78dbabde44
liushuyu <liushuyu011@gmail.com>
2024-01-09 22:58:56 -0700

    fix(polyfill): add polyfill functions

--- native/polyfill_execute_cmd.h
+++ native/polyfill_execute_cmd.h
@@ -3,3 +3,4 @@
 extern int executing_line_number();
 extern volatile int last_command_exit_value;
 extern int execute_command(COMMAND *);
+extern int execute_shell_function(const char *, WORD_LIST *);
