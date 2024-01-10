3e75b34150b6da516a632a378019bb66ea3bb30a
liushuyu <liushuyu011@gmail.com>
2024-01-09 23:44:10 -0700

    fix(polyfill_execute_cmd): fix prototype

--- native/polyfill_execute_cmd.h
+++ native/polyfill_execute_cmd.h
@@ -1,6 +1,8 @@
 #include "unistd.h"
 #include "command.h"
+
+typedef struct variable SHELL_VAR;
 extern int executing_line_number();
 extern volatile int last_command_exit_value;
 extern int execute_command(COMMAND *);
-extern int execute_shell_function(const char *, WORD_LIST *);
+extern int execute_shell_function(SHELL_VAR *, WORD_LIST *);
