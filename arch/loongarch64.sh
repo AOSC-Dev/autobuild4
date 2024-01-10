1a2c6cb9359deb2f27497fdc5c732ee7d0553f67
liushuyu <liushuyu011@gmail.com>
2024-01-09 22:14:28 -0700

    fix(loongarch64): sync with ab3

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,6 +2,8 @@
 ##arch/loongarch64.sh: Build definitions for Loongson 3A/B 5000.
 ##@copyright GPL-2.0+
 
-CFLAGS_GCC_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mtune=la464' '-mlsx' '-flto-partition=none')
+CFLAGS_GCC_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mtune=la464' '-mlsx')
 
-RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=generic-la64' '-Ctarget-feature=+lsx,+d' '-Clink-arg=-mabi=lp64d')
+# FIXME: +lsx feature is broken as of LLVM 17.0.6, awaiting full vector
+# implementation.
+RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=generic-la64 -Ctarget-feature=-lsx,+d -Clink-arg=-mabi=lp64d'
