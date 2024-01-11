7c4bfc0df4ac0788cb87f76c47fb8ce13a62e4c8
liushuyu <liushuyu011@gmail.com>
2024-01-11 09:16:36 -0700

    fix(loongarch64.sh): switch to array syntax

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -6,4 +6,4 @@ CFLAGS_GCC_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mtune=la464' '-mlsx')
 
 # FIXME: +lsx feature is broken as of LLVM 17.0.6, awaiting full vector
 # implementation.
-RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=generic-la64 -Ctarget-feature=-lsx,+d -Clink-arg=-mabi=lp64d'
+RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=generic-la64' '-Ctarget-feature=-lsx,+d' '-Clink-arg=-mabi=lp64d')
