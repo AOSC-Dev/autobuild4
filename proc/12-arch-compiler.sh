7dd74a9481670f18d14cdaaf0873aee85b5531dd
Cyan <cyanoxygen@aosc.io>
2024-01-17 11:11:17 +0800

    proc/12-arch: allow optenv targets

--- proc/12-arch-compiler.sh
+++ proc/12-arch-compiler.sh
@@ -13,6 +13,17 @@ if [[ "$ABBUILD" == "$ABHOST" ]]; then
 			export CC=gcc CXX=g++ OBJC=gcc OBJCXX=g++
 		fi
 	fi
+elif [[ "$ABHOST" == "optenv32" ]]; then
+	if bool "$USECLANG"; then
+		# FIXME: LLVM must be patched to recognize libdirs and
+		# includedirs of optenvs in order to get them to compile.
+		abdie "Sorry, optenv targets currently can not handle USECLANG=1."
+	else
+		export CC=i686-aosc-linux-gnu-gcc CXX=i686-aosc-linux-gnu-g++ \
+			OBJC=i686-aosc-linux-gnu-gcc OBJCXX=i686-aosc-linux-gnu-g++ \
+			LD=i686-aosc-linux-gnu-ld
+	fi
+# Possible optenvx64 in the future.
 elif [[ "$ABHOST" != 'noarch' ]]; then
     abdie "Cross-compilation is no longer supported."
 fi
