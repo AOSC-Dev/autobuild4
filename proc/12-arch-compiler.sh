86cc33fb44c813bbd896a2693e65f17dd2b6e262
Cyan <cyanoxygen@aosc.io>
2024-01-17 11:11:17 +0800

    proc/12-arch: allow optenv32

--- proc/12-arch-compiler.sh
+++ proc/12-arch-compiler.sh
@@ -13,6 +13,15 @@ if [[ "$ABBUILD" == "$ABHOST" ]]; then
 			export CC=gcc CXX=g++ OBJC=gcc OBJCXX=g++
 		fi
 	fi
+elif [[ "$ABHOST" == "optenv32" ]]; then
+	if bool "$USECLANG"; then
+		export CC=clang CXX=clang++ OBJC=clang OBJCXX=clang++
+		# TODO add target flags to Clang, e.g. --target=i686aosc-linux-gnu
+	else
+		export CC=i686-aosc-linux-gnu-gcc CXX=i686-aosc-linux-gnu-g++ \
+			OBJC=i686-aosc-linux-gnu-gcc OBJCXX=i686-aosc-linux-gnu-g++
+	fi
+# Possible optenvx64 in the future.
 elif [[ "$ABHOST" != 'noarch' ]]; then
     abdie "Cross-compilation is no longer supported."
 fi
