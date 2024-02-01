7f7b662effe71df4e8584acd9e7d45e9dcdc6e9c
liushuyu <liushuyu011@gmail.com>
2024-01-31 22:42:09 -0700

    fix(templates/16-pep517.sh): lint script, implement workaround helper
    Co-authored-by: Mingcong Bai <jeffbai@aosc.io>

---
+++ helpers/pep517-helper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import tomli
+import sys
+
+
+def get_build_backend(root_dir: str) -> str:
+    with open(os.path.join(root_dir, "pyproject.toml"), "rb") as f:
+        proj = tomli.load(f)
+        build_sys = proj.get("build-system", {})
+        return build_sys.get("build-backend", "unknown")
+
+
+if __name__ == "__main__":
+    print(get_build_backend(sys.argv[1]))
