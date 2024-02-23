e8072995c7e2acdef48d041619f99343518597ec
liushuyu <liushuyu011@gmail.com>
2024-02-22 21:13:49 -0700

    fix(proc): do not build debug symbol package ...
    ... if ABSPLITDBG is disabled, but debug symbol is present

--- proc/80-pm-pack-install.sh
+++ proc/80-pm-pack-install.sh
@@ -8,7 +8,9 @@ for i in "${ABMPM[@]}"; do
 	abinfo "Packing $i package(s) ..."
 	source "$AB"/pm/"$i".sh || aberr "$i packing returned $?."
     pm_build_package
-    if [[ -d "${SYMDIR}" ]] && [ "$(ls -A "$SYMDIR")" ]; then
+    if bool "$ABSPLITDBG" && \
+       [[ -d "${SYMDIR}" ]] && \
+       [ "$(ls -A "$SYMDIR")" ]; then
         pm_build_debug_package
     elif bool "$ABSPLITDBG"; then
         abdie "ABSPLITDBG is set, but we can't find any symbol files."
