4dcbb153c1b7515a6f48cea4359240bb52e2d44f
Jiangjin Wang <kaymw@aosc.io>
2024-05-22 17:16:44 -0700

    build_spiral_lut: switch to Ubuntu 24.04

--- build_spiral_lut.sh
+++ build_spiral_lut.sh
@@ -2,20 +2,20 @@
 
 set -euo pipefail
 
-DEBIAN_CODENAME="bookworm"
-DEBIAN_CONTENTS="http://ftp.debian.org/debian/dists/${DEBIAN_CODENAME}/main/Contents-amd64.gz"
-DEBIAN_CONTENTS_FILE="$(mktemp)"
+UBUNTU_CODENAME="noble"
+UBUNTU_CONTENTS="http://archive.ubuntu.com/ubuntu/dists/${UBUNTU_CODENAME}/Contents-amd64.gz"
+UBUNTU_CONTENTS_FILE="$(mktemp)"
 
 ROOT_DIR="$(dirname "$0")/data"
 
-SONAME_REGEX='^/?usr/lib/(?:x86_64-linux-gnu/)(?P<key>lib[a-zA-Z0-9\-\._+]+\.so(?:\.[0-9]+)*)\s+[a-zA-Z0-9]+/(?P<value>[a-zA-Z0-9\.\-+]+)$'
+SONAME_REGEX='^/?usr/lib/(?:x86_64-linux-gnu/)(?P<key>lib[a-zA-Z0-9\-\._+]+\.so(?:\.[0-9]+)*)\s+([a-zA-Z0-9]+/){0,2}(?P<value>[a-zA-Z0-9\.\-+]+)$'
 
 function generate_lut {
-	zcat "$1" | rg "$2" -or '$key,$value' > "$3"
+	zcat "$1" | rg "$2" -or '$key,$value' | sort | uniq > "$3"
 }
 
 echo "Downloading Contents-amd64.gz ..."
-curl -fSL "${DEBIAN_CONTENTS}" > "$DEBIAN_CONTENTS_FILE"
+curl -fSL "${UBUNTU_CONTENTS}" > "$UBUNTU_CONTENTS_FILE"
 
 echo "Generating LUT for SONAME lookup to $ROOT_DIR/lut_sonames..."
-generate_lut "$DEBIAN_CONTENTS_FILE" "$SONAME_REGEX" "$ROOT_DIR/lut_sonames"
+generate_lut "$UBUNTU_CONTENTS_FILE" "$SONAME_REGEX" "$ROOT_DIR/lut_sonames"
