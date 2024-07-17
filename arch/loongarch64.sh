5292ed45f9463a66b0b80612797ba5deefe16539
Mingcong Bai <jeffbai@aosc.io>
2024-07-17 22:30:56 +0800

    feat(arch/loongarch64.sh): enable LSX in RUSTFLAGS
    LLVM 18 is here!

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -8,4 +8,4 @@ CFLAGS_CLANG_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mtune=la464')
 
 # FIXME: +lsx feature is broken as of LLVM 17.0.6, awaiting full vector
 # implementation.
-RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=generic-la64' '-Ctarget-feature=-lsx,+d' '-Clink-arg=-mabi=lp64d')
+RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=generic-la64' '-Ctarget-feature=+lsx,+d' '-Clink-arg=-mabi=lp64d')
