6147bb15e5f775873bdec5ae434d89e004588abf
Cyan <cyan@cyano.uk>
2024-07-25 11:33:27 +0800

    arch/amd64: add ASE flags to CPPFLAGS
    - Glibc uses CPP to preprocess their assembly files. With no multiarch
      support, glibc need to select the closest optimized code for each ISA
      level. To make this happen, ISA level flags must be passed to CPP.
      Otherwise, the build does not fail, but no implementation will be
      selected, resulting an linkage error.
    - Also make the comments more informative.
    - Also add SSE3, SSSE3 and AVX flags for supported subtargets.

--- arch/amd64.sh
+++ arch/amd64.sh
@@ -12,15 +12,27 @@ CFLAGS_COMMON_ARCH_BASE=('-fomit-frame-pointer')
 # Default -march and μarch specific tuning.
 # No need to reference CFLAGS_COMMON_ARCH_BASE, it will be concatenated.
 CFLAGS_COMMON_ARCH=('-march=x86-64' '-mtune=sandybridge' '-msse2')
-
-# TODO What about Rust libraries?
 RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=x86-64')
 
-CFLAGS_HWCAPS_x86_64_v2=("${CFLAGS_COMMON_ARCH_BASE[@]}" "-march=x86-64-v2" "-mtune=sandybridge" "-msse2" "-msse4.2")
-CFLAGS_HWCAPS_x86_64_v3=("${CFLAGS_COMMON_ARCH_BASE[@]}" "-march=x86-64-v3" "-mtune=haswell" "-msse2" "-msse4.2" "-mavx2")
+# List of HWCAPS subtargets in x86-64:
+#
+# - x86-64-v2: CPUs with CMPXCHG16B, SSE3, SSSE3 and SSE4.2 extensions,
+#   starting from Sandy Bridge.
+# - x86-64-v3: CPUs with AVX, AVX2, BMI1, BMI2, and other miscellaneous
+#   instructions, staring from Haswell. Also the most common one.
+# - x86-64-v4: CPUs with AVX512 (AVX-512-F, AVX-512-BW, AVX-512-CD,
+#   AVX-512-DQ and AVX-512-DL specifically). Few consumer-grade CPUs
+#   supports this.
+CFLAGS_HWCAPS_x86_64_v2=("${CFLAGS_COMMON_ARCH_BASE[@]}" "-march=x86-64-v2" "-mtune=sandybridge" "-msse2" "-msse3" "-mssse3" "-msse4.2")
+CFLAGS_HWCAPS_x86_64_v3=("${CFLAGS_COMMON_ARCH_BASE[@]}" "-march=x86-64-v3" "-mtune=haswell" "-msse2" "-msse3" "-mssse3" "-msse4.2" "-mavx" "-mavx2")
 # TBD: Which -mtune to be chosen for x86_64 v4?
-CFLAGS_HWCAPS_x86_64_v4=("${CFLAGS_COMMON_ARCH_BASE[@]}" "-march=x86-64-v4" "-mtune=generic" "-msse2" "-msse4.2" "-mavx2" "-mavx512f" "-mavx512bw" "-mavx512cd" "-mavx512dq" "-mavx512vl")
+CFLAGS_HWCAPS_x86_64_v4=("${CFLAGS_COMMON_ARCH_BASE[@]}" "-march=x86-64-v4" "-mtune=generic" "-msse2" "-msse3" "-mssse3" "-msse4.2" "-mavx" "-mavx2" "-mavx512f" "-mavx512bw" "-mavx512cd" "-mavx512dq" "-mavx512vl")
 
 RUSTFLAGS_HWCAPS_x86_64_v2=("-Ctarget-cpu=x86-64-v2")
 RUSTFLAGS_HWCAPS_x86_64_v3=("-Ctarget-cpu=x86-64-v3")
 RUSTFLAGS_HWCAPS_x86_64_v4=("-Ctarget-cpu=x86-64-v4")
+
+# CPPFLAGS must be set for function selection in preprocessing to work.
+CPPFLAGS_HWCAPS_x86_64_v2=("-march=x86-64-v2" "-msse2" "-msse3" "-mssse3" "-msse4.2")
+CPPFLAGS_HWCAPS_x86_64_v3=("-march=x86-64-v3" "-msse2" "-msse3" "-mssse3" "-msse4.2" "-mavx2")
+CPPFLAGS_HWCAPS_x86_64_v4=("-march=x86-64-v4" "-msse2" "-msse3" "-mssse3" "-msse4.2" "-mavx2" "-mavx512f" "-mavx512bw" "-mavx512cd" "-mavx512dq" "-mavx512vl")
