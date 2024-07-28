2fd142548ea466114203d0f49d0003500394209a
Kexy Biscuit <kexybiscuit@aosc.io>
2024-07-28 14:45:38 +0800

    feat(arch/loongarch64.sh): enable LSX for Clang

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,10 +2,5 @@
 ##arch/loongarch64.sh: Build definitions for LoongArch targets.
 ##@copyright GPL-2.0+
 
-CFLAGS_GCC_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mtune=la464' '-mlsx')
-# FIXME: -mlsx is not yet available as of LLVM 17.0.6.
-CFLAGS_CLANG_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mtune=la464')
-
-# FIXME: +lsx feature is broken as of LLVM 17.0.6, awaiting full vector
-# implementation.
+CFLAGS_COMMON_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mtune=la464' '-mlsx')
 RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=generic-la64' '-Ctarget-feature=+lsx,+d' '-Clink-arg=-mabi=lp64d')
