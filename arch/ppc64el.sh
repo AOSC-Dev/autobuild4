0475e66ede6e3599a5c411c639920e0511111d19
Mingcong Bai <jeffbai@aosc.io>
2024-07-29 21:14:40 +0800

    fix(arch/ppc64el.sh): merge CFLAGS_{CLANG,GCC}_ARCH as CFLAGS_COMMON_ARCH
    The two sets of flags are identical.

--- arch/ppc64el.sh
+++ arch/ppc64el.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ##arch/ppc64el.sh: Build definitions for ppc64el.
 ##@copyright GPL-2.0+
-CFLAGS_GCC_ARCH=('-mcpu=power8' '-mtune=power9' '-msecure-plt' '-mvsx' '-mabi=ieeelongdouble')
-CFLAGS_CLANG_ARCH=('-mcpu=power8' '-mtune=power9' '-msecure-plt' '-mvsx' '-mabi=ieeelongdouble')
+CFLAGS_COMMON_ARCH=('-mcpu=power8' '-mtune=power9' '-msecure-plt' '-mvsx' '-mabi=ieeelongdouble')
 RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=pwr8' '-Ctarget-feature=+vsx,+altivec,+secure-plt' '-Clink-arg=-mabi=ieeelongdouble')
