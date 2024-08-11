b8cfc42253cf6dd58183a7ac0f482d1385542ee3
eatradish <sakiiily@aosc.io>
2024-08-11 11:24:28 +0800

    sets/export: export `PKGEPOCH` var

--- proc/01-core-defines.sh
+++ proc/01-core-defines.sh
@@ -61,6 +61,7 @@ if [[ $ABHOST == noarch ]]; then
 fi
 
 abisdefined PKGREL || PKGREL=0
+abisdefined PKGEPOCH || PKGEPOCH=0
 
 load_strict "$AB/arch/_common_switches.sh"
 load_strict "$AB"/lib/builtin.sh
