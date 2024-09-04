1e41a63b6751db5d9323c4634debd2faa48daca3
Mingcong Bai <jeffbai@aosc.io>
2024-09-04 11:23:05 +0800

    feat(proc/10-env-setup): add /usr/bin/core_perl to PATH
    Perl installs its core scripts in /usr/bin/core_perl. We want to minimise
    profile script loading, so handle it here.

--- proc/10-env-setup.sh
+++ proc/10-env-setup.sh
@@ -7,6 +7,11 @@
 abinfo 'Setting $HOME to /root ...'
 export HOME="/root"
 
+# Note: Perl installs its core scripts in /usr/bin/core_perl.
+# We want to minimise profile script loading, so handle it here.
+abinfo 'Setting up /usr/bin/core_perl as part of the $PATH ...'
+export PATH="$PATH:/usr/bin/core_perl"
+
 if [ -e /usr/lib/hook_uname.so ]; then
     abinfo 'hook_uname.so detected, preloading ...'
     export LD_PRELOAD="/usr/lib/hook_uname.so:$LD_PRELOAD"
