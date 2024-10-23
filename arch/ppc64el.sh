aaad4c57a0291e80ffdb03bf9c7aac4b7a47a16a
Cyan <cyan@cyano.uk>
2024-10-23 18:05:02 +0800

    arch/ppc64el: add HWCAPS support
    - Infrom the HWCAPS support.
    - Define compiler flags for each HWCAPS subtarget.
    - Split non-arch-specific compiler flags into COMMON_ARCH_BASE.

--- arch/ppc64el.sh
+++ arch/ppc64el.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 ##arch/ppc64el.sh: Build definitions for ppc64el.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=('-mcpu=power8' '-mtune=power9' '-msecure-plt' '-mvsx' '-mabi=ieeelongdouble')
+CFLAGS_COMMON_ARCH_BASE=('-msecure-plt' '-mvsx' '-mabi=ieeelongdouble')
+CFLAGS_COMMON_ARCH=('-mcpu=power8' '-mtune=power8')
 RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=pwr8' '-Ctarget-feature=+vsx,+altivec,+secure-plt' '-Clink-arg=-mabi=ieeelongdouble')
+
+HAS_HWCAPS=1
+# ppc64el only supports power8 and power9. The default is power8.
+HWCAPS=('power9')
+CFLAGS_HWCAPS_power9=("${CFLAGS_COMMON_ARCH_BASE[@]}" '-mcpu=power9' '-mtune=power9')
+CPPFLAGS_HWCAPS_power9=("${CFLAGS_COMMON_ARCH_BASE[@]}" '-mcpu=power9' '-mtune=power9')
+RUSTFLAGS_HWCAPS_power9=('-Ctarget-cpu=pwr9' '-Ctarget-feature=+vsx,+altivec,+secure-plt' '-Clink-arg=-mabi=ieeelongdouble')
