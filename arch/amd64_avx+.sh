cd7b7c2bb6bc04a0d1630733c40c9739baf3d7f3
Mingcong Bai <jeffbai@aosc.io>
2024-12-01 20:50:21 +0800

    arch: drop amd64_avx* overlay targets
    We have adopted glibc HWCAPS subdirectories as part of Core 12.

--- arch/amd64_avx+.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-##arch/amd64_avx.sh: Build definitions for amd64 with AVX support.
-##                   Intel SandyBridge and AMD Bulldozer or later processors.
-##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=('-march=sandybridge')
-RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=sandybridge')
--- arch/amd64_avx2+.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-##arch/amd64_avx2.sh: Build definitions for amd64 with AVX2 support.
-##                    Intel Haswell+, AMD bdver4+, VIA eden-x4+.
-##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=('-march=haswell' '-mno-rdrnd')
-RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=haswell' '-Ctarget-feature=-rdrnd')
