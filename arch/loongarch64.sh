1954e9fc1f248a2c105f9c493c5f542bd26ade5b
Mingcong Bai <jeffbai@aosc.io>
2024-12-01 21:33:27 +0800

    arch/loongarch64.sh: tune for LA664 cores
    However, the `la664' target is only available on GCC >= 14, so write a
    simple if-branch to detect GCC versions.

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,5 +2,14 @@
 ##arch/loongarch64.sh: Build definitions for LoongArch targets.
 ##@copyright GPL-2.0+
 
-CFLAGS_COMMON_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mtune=la464' '-mlsx')
+# la664 is only available on GCC >= 14.
+CFLAGS_COMMON_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mlsx')
+if [ "$(echo __GNUC__ | gcc -E -xc - | tail -n 1)" -ge 14 ]; then
+	# Use `-mtune=la664' if we are on GCC >= 14.
+	CFLAGS_COMMON_ARCH+=('-mtune=la664')
+else
+	# Fallback to `-mtune=la464' if we are not.
+	CFLAGS_COMMON_ARCH+=('-mtune=la464')
+fi
+
 RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=generic-la64' '-Ctarget-feature=+lsx,+d' '-Clink-arg=-mabi=lp64d')
