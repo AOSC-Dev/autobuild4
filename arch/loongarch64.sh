898b1ba9113917cf042e907b161544dcdb66b5f4
Mingcong Bai <jeffbai@aosc.io>
2024-12-02 14:09:13 +0800

    arch/loongarch64.sh: allow -march=la664 on Clang >= 19

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,12 +2,13 @@
 ##arch/loongarch64.sh: Build definitions for LoongArch targets.
 ##@copyright GPL-2.0+
 
-# la664 is only available on GCC >= 14.
+# la664 is only available on GCC >= 14 and Clang >= 19.
 CFLAGS_COMMON_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mlsx')
-if [ "$(echo __GNUC__ | gcc -E -xc - | tail -n 1)" -ge 14 ]; then
-	# Use `-mtune=la664' if we are on GCC >= 14.
+if [ "$(echo __GNUC__ | gcc -E -xc - | tail -n 1)" -ge 14 ] || \
+   [ "$(__clang_major__ | clang -E -x c - | tail -n 1)" -ge 19 ]; then
+	# Use `-mtune=la664' if we are on GCC >= 14 or Clang >= 19.
 	CFLAGS_COMMON_ARCH+=('-mtune=la664')
-else
+elif
 	# Fallback to `-mtune=la464' if we are not.
 	CFLAGS_COMMON_ARCH+=('-mtune=la464')
 fi
