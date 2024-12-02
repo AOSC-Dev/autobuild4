59ef883a8b6442a9ed5a9eb7975a389cf80f5d99
Xinhui Yang <cyan@cyano.uk>
2024-12-02 18:07:41 +0800

    arch: fix conditional parameters application
    - Limiting the condition by detecting the compiler being used. GCC
      checks must be performed only if not using Clang (USECLANG=0), and
      vice versa.
    - Fix a typo leaving the condition list as "if ... elif ... fi".

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -4,11 +4,13 @@
 
 # la664 is only available on GCC >= 14 and Clang >= 19.
 CFLAGS_COMMON_ARCH=('-mabi=lp64d' '-march=loongarch64' '-mlsx')
-if [ "$(echo __GNUC__ | gcc -E -xc - | tail -n 1)" -ge 14 ] || \
-   [ "$(__clang_major__ | clang -E -x c - | tail -n 1)" -ge 19 ]; then
-	# Use `-mtune=la664' if we are on GCC >= 14 or Clang >= 19.
+if ! bool "$USECLANG" && [ "$(echo __GNUC__ | gcc -E -xc - | tail -n 1)" -ge 14 ]; then
+	# Use `-mtune=la664' if we are on GCC >= 14.
 	CFLAGS_COMMON_ARCH+=('-mtune=la664')
-elif
+elif bool "$USECLANG" && [ "$(echo __clang_major__ | clang -E -x c - | tail -n 1)" -ge 19 ]; then
+	# Use `-mtune=la664' if we are on Clang >= 19.
+	CFLAGS_COMMON_ARCH+=('-mtune=la664')
+else
 	# Fallback to `-mtune=la464' if we are not.
 	CFLAGS_COMMON_ARCH+=('-mtune=la464')
 fi
--- arch/riscv64.sh
+++ arch/riscv64.sh
@@ -3,7 +3,7 @@
 ##@copyright GPL-2.0+
 
 # `-mno-omit-leaf-frame-pointer' is only available on GCC >= 14.
-if [ "$(echo __GNUC__ | gcc -E -xc - | tail -n 1)" -ge 14 ]; then
+if ! bool "$USECLANG" && [ "$(echo __GNUC__ | gcc -E -xc - | tail -n 1)" -ge 14 ]; then
 	CFLAGS_COMMON_ARCH=('-mno-omit-leaf-frame-pointer')
 fi
 LDFLAGS_COMMON_CROSS=('-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link' '-Wl,/var/ab/cross-root/riscv64/usr/lib' '-L/var/ab/cross-root/riscv64/usr/lib')
