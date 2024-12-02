3c56fc6a9e8d65099e2689343a70ceb14f4674c6
Mingcong Bai <jeffbai@aosc.io>
2024-12-02 14:07:37 +0800

    arch/riscv64.sh: specify -mno-omit-leaf-frame-pointer on GCC >= 14 only

--- arch/riscv64.sh
+++ arch/riscv64.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ##arch/riscv64.sh: Build definitions for riscv64.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=('-mno-omit-leaf-frame-pointer')
+
+# `-mno-omit-leaf-frame-pointer' is only available on GCC >= 14.
+if [ "$(echo __GNUC__ | gcc -E -xc - | tail -n 1)" -ge 14 ]; then
+	CFLAGS_COMMON_ARCH=('-mno-omit-leaf-frame-pointer')
+fi
 LDFLAGS_COMMON_CROSS=('-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link' '-Wl,/var/ab/cross-root/riscv64/usr/lib' '-L/var/ab/cross-root/riscv64/usr/lib')
 RUSTFLAGS_COMMON_ARCH=("-Clink-arg=-mabi=lp64d")
 RUSTFLAGS_COMMON_ARCH_LTO=("${RUSTFLAGS_COMMON_ARCH[@]}" '-Clink-arg=-Wl,-mllvm=-mattr=+d')
