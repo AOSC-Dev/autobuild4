bdb9f94fd393f8f67ea79f8ac933e82ecf060f74
Mingcong Bai <jeffbai@aosc.io>
2024-12-14 20:15:38 +0800

    arch: limit la64v1.0 target to GCC >= 14 and Clang >= 19

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -20,17 +20,17 @@
 # Our default flags comply with the Convention.
 #
 # Ref: https://github.com/loongson/la-softdev-convention/blob/v0.2/la-softdev-convention.adoc
-CFLAGS_COMMON_ARCH=('-mabi=lp64d' '-march=la64v1.0' '-mno-strict-align')
-# la664 is only available on GCC >= 14 and Clang >= 19.
-if ! bool "$USECLANG" && [ "$(echo __GNUC__ | $CC -E -xc - | tail -n 1)" -ge 14 ]; then
-	# Use `-mtune=la664' if we are on GCC >= 14.
-	CFLAGS_COMMON_ARCH+=('-mtune=la664')
-elif bool "$USECLANG" && [ "$(echo __clang_major__ | $CC -E -x c - | tail -n 1)" -ge 19 ]; then
-	# Use `-mtune=la664' if we are on Clang >= 19.
-	CFLAGS_COMMON_ARCH+=('-mtune=la664')
+CFLAGS_COMMON_ARCH=('-mabi=lp64d' '-mno-strict-align')
+# la64v1.0/la664 targets are only available on GCC >= 14 and Clang >= 19.
+if ! bool "$USECLANG" && \
+     [ "$(echo __GNUC__ | $CC -E -xc - | tail -n 1)" -ge 14 ]; then
+	CFLAGS_COMMON_ARCH+=('-march=la64v1.0' '-mtune=la664')
+elif bool "$USECLANG" && \
+     [ "$(echo __clang_major__ | $CC -E -x c - | tail -n 1)" -ge 19 ]; then
+	CFLAGS_COMMON_ARCH+=('-march=la64v1.0' '-mtune=la664')
 else
-	# Fallback to `-mtune=la464' if we are not.
-	CFLAGS_COMMON_ARCH+=('-mtune=la464')
+	# Fallback to `-march=loongarch64' and `-mtune=la464' if we are not.
+	CFLAGS_COMMON_ARCH+=('-march=loongarch64' '-mtune=la464')
 fi
 
 RUSTFLAGS_COMMON_ARCH=('-Ctarget-cpu=generic-la64' '-Ctarget-feature=+lsx,+d' '-Clink-arg=-mabi=lp64d')
