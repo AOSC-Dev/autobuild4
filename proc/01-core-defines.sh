ce849e51761748b0e769f038f1a655b1115b0d7e
Mingcong Bai <jeffbai@aosc.io>
2024-12-22 17:10:18 +0800

    proc: move arch-compiler logic into core-defines
    Such that references to $CC will work for version-specific flags.

--- proc/01-core-defines.sh
+++ proc/01-core-defines.sh
@@ -31,6 +31,32 @@ ab_parse_set_modifiers
 
 arch_loaddefines -2 defines || abdie "Failed to source defines file: $?."
 
+if [[ "$ABBUILD" == "$ABHOST" ]]; then
+	if [ -d /opt/pseudo-multilib/bin ]; then
+		export PATH="/opt/pseudo-multilib/bin:$PATH"
+	else
+		if bool "$USECLANG"; then
+			export CC=clang CXX=clang++ OBJC=clang OBJCXX=clang++
+		else
+			export CC=gcc CXX=g++ OBJC=gcc OBJCXX=g++
+		fi
+	fi
+elif [[ "$ABHOST" == "optenv32" ]]; then
+	if bool "$USECLANG"; then
+		# FIXME: LLVM must be patched to recognize libdirs and
+		# includedirs of optenvs in order to get them to compile.
+		abdie "Sorry, optenv targets currently can not handle USECLANG=1."
+	else
+		export \
+			CC=i686-aosc-linux-gnu-gcc CXX=i686-aosc-linux-gnu-g++ \
+			OBJC=i686-aosc-linux-gnu-gcc OBJCXX=i686-aosc-linux-gnu-g++ \
+			LD=i686-aosc-linux-gnu-ld
+	fi
+# Possible optenvx64 in the future.
+elif [[ "$ABHOST" != 'noarch' ]]; then
+	abdie "Cross-compilation is no longer supported."
+fi
+
 load_strict "$AB/arch/${ABHOST//\//_}.sh"
 load_strict "$AB"/lib/default-flags.sh
 
