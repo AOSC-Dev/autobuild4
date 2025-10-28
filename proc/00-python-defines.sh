7d72822433c76b1c366d9082cded2b3936df5219
Mingcong Bai <jeffbai@aosc.io>
2025-10-28 12:58:50 +0800

    fix(proc/00-python-defines): guard ABPY*VER assignment with command -v
    Lest Autobuild throws an error when either python2 or python3 does not
    exist (especially now that the former is gone from BuildKit).

--- proc/00-python-defines.sh
+++ proc/00-python-defines.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 ##python_defines: Dynamic Python version declaration for installation scripts
 ##@copyright GPL-2.0+
-export ABPY2VER="$(python2 -c 'import sys; print("%s.%s" %sys.version_info[0:2])')"
-export ABPY2SHORTVER="$(python2 -c 'import sys; print("%s%s" %sys.version_info[0:2])')"
-export ABPY3VER="$(python3 -c 'import sys; print("%s.%s" %sys.version_info[0:2])')"
-export ABPY3SHORTVER="$(python3 -c 'import sys; print("%s%s" %sys.version_info[0:2])')"
+if command -v python2 > /dev/null; then
+	export ABPY2VER="$(python2 -c 'import sys; print("%s.%s" %sys.version_info[0:2])')"
+	export ABPY2SHORTVER="$(python2 -c 'import sys; print("%s%s" %sys.version_info[0:2])')"
+fi
+if command -v python3 > /dev/null; then
+	export ABPY3VER="$(python3 -c 'import sys; print("%s.%s" %sys.version_info[0:2])')"
+	export ABPY3SHORTVER="$(python3 -c 'import sys; print("%s%s" %sys.version_info[0:2])')"
+fi
 
 export PYTHON=/usr/bin/python3
