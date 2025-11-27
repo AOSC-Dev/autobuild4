dbf223b526362592595c35f49187c18fb2272ef9
Icenowy Zheng <uwu@icenowy.me>
2025-11-27 18:18:31 +0800

    arch/riscv64.sh: Disable emitting fence.tso on GCC 15+
    GCC 15 introduces a new compiler flag, `-m{,no-}fence-tso`, to control
    the usage of `fence.tso` instruction (which is not supported on some old
    T-Head cores, such as the ones used on TH1520/SG2042). When it's
    disabled, more strong `fence rw,rw` is emitted and correctness is
    then not affected.
    
    Disable emitting this instruction now, because all buildbots of riscv64
    are currently SG2042, and no CPU cores are known to benefit from
    `fence.tso` yet.
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>

--- arch/riscv64.sh
+++ arch/riscv64.sh
@@ -6,6 +6,10 @@
 if ! bool "$USECLANG" && [ "$(echo __GNUC__ | $CC -E -xc - | tail -n 1)" -ge 14 ]; then
 	CFLAGS_COMMON_ARCH=('-mno-omit-leaf-frame-pointer')
 fi
+# `-mno-fence-tso` is only available on GCC >= 15
+if ! bool "$USECLANG" && [ "$(echo __GNUC__ | $CC -E -xc - | tail -n 1)" -ge 15 ]; then
+	CFLAGS_COMMON_ARCH+=('-mno-fence-tso')
+fi
 LDFLAGS_CLANG_ARCH=('-fuse-ld=lld' '-Wl,-mllvm=-mattr=+rva20u64')
 LDFLAGS_COMMON_CROSS=('-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link' '-Wl,/var/ab/cross-root/riscv64/usr/lib' '-L/var/ab/cross-root/riscv64/usr/lib')
 RUSTFLAGS_COMMON_ARCH=("-Clink-arg=-mabi=lp64d")
