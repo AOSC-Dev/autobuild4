76f40d4d350b19c54cd4d208e026f9b0e9838178
liushuyu <liushuyu011@gmail.com>
2026-02-15 18:48:35 +0800

    native/abspiral_data: resort to std::unordered_map to ...
    ... fix a potential miscompile that causes SPIRAL data to be corrupted

--- native/abspiral_data.cpp
+++ native/abspiral_data.cpp
@@ -1,15 +1,22 @@
 #define MAPBOX_ETERNAL_UNSAFE_ASSUME_SORTED
 #include "eternal.hpp"
 
+#include <unordered_map>
+
 constexpr std::pair<const mapbox::eternal::string,
                     const mapbox::eternal::string>
     data[] = {
-      #include "../data/lut_sonames.cpp.inc"
-    };
-MAPBOX_ETERNAL_CONSTEXPR auto spiral_lut = mapbox::eternal::map(data);
+#include "../data/lut_sonames.cpp.inc"
+};
+MAPBOX_ETERNAL_CONSTEXPR std::unordered_map<mapbox::eternal::string,
+                                            mapbox::eternal::string>
+    spiral_lut =
+        std::unordered_map<mapbox::eternal::string, mapbox::eternal::string>(
+            data, data + sizeof(data) / sizeof(data[0]));
 
-const char* lut_lookup(const char* soname) {
-    const auto found = spiral_lut.find(soname);
-    if (found == spiral_lut.end()) return nullptr;
-    return found->second.c_str();
+const char *lut_lookup(const char *soname) {
+  const auto found = spiral_lut.find(soname);
+  if (found == spiral_lut.end())
+    return nullptr;
+  return found->second.c_str();
 }
