507b5d8f38d8404ac411c0e4eb9b478b74571343
Student Main <studentmain@aosc.io>
2026-04-15 19:40:22 +0800

    filters: add PDB file filter
    Program Database (PDB) file contains debug symbol for PE binary,
    which can be found in most .NET softwares.

---
+++ filters/81-pdb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##filter/pdb.sh: Move or delete Program Database file
+##@copyright GPL-2.0+
+
+filter_pdb() {
+	for p in ${BIN_DIRS[@]}; do
+        i="$PKGDIR"/"$p"
+	    if [ -d "$i" ]; then
+            for f in `find $i -type f -name "*.pdb"`; do
+                if bool "$ABSPLITDBG"; then
+                    path="${f#"$PKGDIR"}"
+                    abinfo "Saving Program Database file $f ..."
+                    mkdir -p `dirname "$SYMDIR"/"$path"`
+                    mv "$f" "$SYMDIR"/"$path"
+                elif bool "$ABSTRIP"; then
+                    abinfo "Dropping Program Database file $f ..."
+                    rm "$f"
+                fi
+            done
+        fi
+	done
+}
+
+ab_register_filter pdb
