00aa00f26dccd345363fe09fe391cd620a39594e
Xinhui Yang <cyan@cyano.uk>
2026-06-18 09:59:55 +0800

    filters/retro-drop-docs: drop unneeded manual pages
    We should only keep manual pages in section 1 (User Commands), section 5
    (File Formats), Section 6 (Games), Section 7 (Conventions and Misc.) and
    Section 8 (Administrative Commands).

--- filters/20-retro_drop_docs.sh
+++ filters/20-retro_drop_docs.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 ##filter/retro_drop_docs.sh: Drop /usr/share/{,gtk-}doc for Retro architectures.
 ##@copyright GPL-2.0+
+
+# Drop any manpage that is not in Section 1 (User Commands), Section 5
+# (File Formants), Section 6 (Games), Section 7 (conventions) and
+# Section 8 (Administrative Commands).
+_retro_drop_manpages() {
+	local tgt="$1"
+	if [ -z "$tgt" ] ; then
+		tgt="$PKGDIR/usr/share/man"
+	fi
+	for dir in `find "$tgt" -mindepth 1 -maxdepth 1 -type d -printf '%P\n'` ; do
+		# Check the translations first: /usr/share/man/$LOCALE
+		if [ "${dir##man}" == "$dir" ] ; then
+			_retro_drop_manpages "$tgt"/"$dir"
+			continue
+		fi
+		if [ "${dir%%[15678]}" == "$dir" ] ; then
+			for manfile in `find "$tgt"/"$dir" -mindepth 1 -not -type d` ; do
+				abinfo "Removing $manfile ..."
+				rm "$manfile"
+			done
+			abinfo "Removing $tgt/$dir ..."
+			rm -rv "$tgt/$dir"
+		fi
+	done
+}
+
 filter_retro_drop_docs() {
 	if ab_match_archgroup retro; then
 		if [ -d "$PKGDIR"/usr/share/doc ]; then
@@ -14,6 +40,11 @@ filter_retro_drop_docs() {
 			-d "$PKGDIR"/usr/share/gtk-doc ]]; then
 			abinfo "Dropping gtk-doc ..."
 			rm -r "$PKGDIR"/usr/share/gtk-doc
+		fi
+
+		# Drop any unnecessary manual pages.
+		if [ -d "$PKGDIR"/usr/share/man ] ; then
+			_retro_drop_manpages "$PKGDIR"/usr/share/man
 		fi
 	else
 		abinfo "Non-Retro architectures detected, skipping retro_drop_docs ..."
