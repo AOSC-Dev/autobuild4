9ef2d3c66785825cff4894cbeef7de66ab0378cf
Rong Bao <rong.bao@csmantle.top>
2026-07-31 20:27:35 +0800

    filters/81-pdb.sh: eliminate ShellCheck warnings
    - Quoted various variables.
    - Switched to $(...) from legacy backticks.
    - Replaced find(1) with a glob pattern.

--- filters/81-pdb.sh
+++ filters/81-pdb.sh
@@ -3,14 +3,14 @@
 ##@copyright GPL-2.0+
 
 filter_pdb() {
-	for p in ${BIN_DIRS[@]}; do
+    for p in "${BIN_DIRS[@]}"; do
         i="$PKGDIR"/"$p"
-	    if [ -d "$i" ]; then
-            for f in `find "$i" -type f -name "*.pdb"`; do
+        if [ -d "$i" ]; then
+            for f in "$i"/**/*.pdb; do
                 if bool "$ABSPLITDBG"; then
                     path="${f#"$PKGDIR"}"
                     abinfo "Saving Program Database file $f ..."
-                    mkdir -p `dirname "$SYMDIR"/"$path"`
+                    mkdir -p "$(dirname "$SYMDIR"/"$path")"
                     mv "$f" "$SYMDIR"/"$path"
                 elif bool "$ABSTRIP"; then
                     abinfo "Dropping Program Database file $f ..."
@@ -18,7 +18,7 @@ filter_pdb() {
                 fi
             done
         fi
-	done
+    done
 }
 
 ab_register_filter pdb
