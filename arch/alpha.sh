03a9a917db3f08a088ea5aca4003a1fa4dfa6e80
Mingcong Bai <jeffbai@aosc.io>
2026-08-08 00:14:34 +0800

    arch/alpha: tune for EV67 (AlphaStation DS10)

--- arch/alpha.sh
+++ arch/alpha.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##arch/alpha.sh: Build definitions for DEC Alpha.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=('-mieee' '-mcpu=ev4')
+CFLAGS_COMMON_ARCH=('-mieee' '-mcpu=ev4' '-mtune=ev67')
 # Retro: Overriding mainline definitions, and take more interest in reducing code size.
 CFLAGS_COMMON_ARCH+=('-O2')
 CFLAGS_GCC_ARCH=('-fno-tree-ch')
