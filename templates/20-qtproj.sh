ebffc7f7433498d6e73db31f9022cfd4a7c1ce7c
Kaiyang Wu <origincode@aosc.io>
2026-09-13 01:22:15 +0800

    templates/qtproj: use QMAKEVER to select qmake version
    The qtproj template did not allow building with Qt 6's qmake.
    
    Remove the unused `QT_SELECT` variable since we no longer ship
    `qtchooser`. Introduce a new variable `QMAKEVER` to specify the `qmake`
    version. Require `QMAKEVER` to be set to avoid ambiguity.
    
    Signed-off-by: Kaiyang Wu <origincode@aosc.io>

--- templates/20-qtproj.sh
+++ templates/20-qtproj.sh
@@ -12,24 +12,22 @@ build_qtproj_probe() {
 }
 
 build_qtproj_configure() {
-	export QT_SELECT
-	[ "$QT_SELECT" ] ||
-	if bool "$USEQT5"; then
-		abwarn "\$USEQT5 is now deprecated. Use QT_SELECT=5."
-		QT_SELECT=5
-	elif bool "$USEQT4"; then
-		abwarn "\$USEQT4 is now deprecated. Use QT_SELECT=4."
-		QT_SELECT=4
-	else
-		abicu "Qt version unspecified => default."
-		QT_SELECT=default
-	fi
-
 	BUILD_START
+	[[ -v "QMAKEVER" ]] || abdie "qmake version unspecified. Set QMAKEVER=<version> to the corresponding Qt version."
+	export QMAKE
+	if [ "$QMAKEVER" -eq 6 ]; then
+		QMAKE="qmake-qt6"
+	elif [ "$QMAKEVER" -eq 5 ]; then
+		QMAKE="qmake-qt5"
+	elif [ "$QMAKEVER" -eq 4 ]; then
+		QMAKE="qmake-qt4"
+	else
+		abdie "Unknown qmake version: ${QMAKEVER}."
+	fi
 	ab_tostringarray QTPROJ_AFTER
 	ab_typecheck -a QTPROJ_DEF
-	abinfo "Running qmake to generate Makefile ..."
-	"$QTPREFIX/bin/qmake" "${QTPROJ_DEF[@]}" "${QTPROJ_AFTER[@]}" \
+	abinfo "Running ${QMAKE} to generate Makefile ..."
+	"/usr/bin/${QMAKE}" "${QTPROJ_DEF[@]}" "${QTPROJ_AFTER[@]}" \
 		|| abdie "Failed while running qmake to generate Makefile: $?."
 }
 
