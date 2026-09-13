1a87642c31b924d6c950be8d8efa9a9fc5209917
Mingcong Bai <jeffbai@aosc.io>
2026-09-13 12:30:51 +0800

    arch/alpha.sh: align better with Afterglow conventions

--- arch/alpha.sh
+++ arch/alpha.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 ##arch/alpha.sh: Build definitions for DEC Alpha.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=('-mieee' '-mcpu=ev4' '-mtune=ev67')
+
 # Retro: Overriding mainline definitions, and take more interest in reducing code size.
-CFLAGS_COMMON_ARCH+=('-O2')
+CFLAGS_COMMON_ARCH=('-ffunction-sections' '-fdata-sections')
 CFLAGS_GCC_ARCH=('-fno-tree-ch')
+LDFLAGS_COMMON_ARCH=('-Wl,--gc-sections')
+
+# Retro: Overriding mainline definitions, and take more interest in reducing code size.
+CFLAGS_COMMON_ARCH+=('-mieee' '-mcpu=ev4' '-mtune=ev67')
+
 # NOTE: Despite the stack growing downwards, they hardcoded
 # FRAME_GROWS_DOWNWARD to 0, so no stack protector support.
 AB_FLAGS_SSP=0
